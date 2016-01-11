5038b8d80ca252cd6832dced35603713e46ea173
Pablo Roulet <roulet@gmail.com>
2016-01-11 00:06:00 -0300

    Read and write to a shared preference

--- app/src/main/java/com/pabrou/widgettest/ExampleAppWidgetProvider.java
+++ app/src/main/java/com/pabrou/widgettest/ExampleAppWidgetProvider.java
@@ -5,8 +5,11 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.RemoteViews;
+
+import java.util.Random;
 
 /**
  * Created by pablo on 10/1/16.
@@ -20,21 +23,35 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
         // Perform this loop procedure for each App Widget that belongs to this provider
         final int N = appWidgetIds.length;
         for (int i=0; i<N; i++) {
-            updateWidget(context, appWidgetManager, appWidgetIds[i]);
+            updateWidget(context, appWidgetManager, appWidgetIds[i], appWidgetIds);
         }
 
     }
 
-    private void updateWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+    private void updateWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId, int[] appWidgetIds) {
 
         // Create an Intent to launch ExampleActivity
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
+        Intent intentUpdate = new Intent(context, ExampleAppWidgetProvider.class);
+        intentUpdate.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        intentUpdate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+        PendingIntent pendingUpdateIntent = PendingIntent.getBroadcast(context,
+                0, intentUpdate, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        String number = String.format("%03d", (new Random().nextInt(900) + 100));
+
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                context.getString(R.string.shared_preference_key), Context.MODE_PRIVATE);
+        String readString = sharedPref.getString(context.getString(R.string.pref_value), "");
+
         // Get the layout for the App Widget and attach an on-click listener
         // to the button
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_appwidget);
         views.setOnClickPendingIntent(R.id.launch_app, pendingIntent);
+        views.setTextViewText(R.id.text_input, "Texto leido:"+readString);
+        views.setOnClickPendingIntent(R.id.update_widget, pendingUpdateIntent);
 
         // Tell the AppWidgetManager to perform an update on the current app widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
--- app/src/main/java/com/pabrou/widgettest/MainActivityFragment.java
+++ app/src/main/java/com/pabrou/widgettest/MainActivityFragment.java
@@ -1,15 +1,25 @@
 package com.pabrou.widgettest;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private TextView outputView;
+    private EditText inputView;
+    private Button saveButton;
 
     public MainActivityFragment() {
     }
@@ -18,5 +28,31 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        SharedPreferences sharedPref = getActivity().getSharedPreferences(
+                getActivity().getString(R.string.shared_preference_key), Context.MODE_PRIVATE);
+
+        String readString = sharedPref.getString(getString(R.string.pref_value), "");
+
+        outputView = (TextView) getActivity().findViewById(R.id.text_output);
+        inputView = (EditText) getActivity().findViewById(R.id.text_input);
+        saveButton = (Button) getActivity().findViewById(R.id.save_button);
+
+        outputView.setText(readString);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = getActivity().getSharedPreferences(
+                        getActivity().getString(R.string.shared_preference_key), Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString(getString(R.string.pref_value), inputView.getText().toString());
+                editor.commit();
+            }
+        });
     }
 }
